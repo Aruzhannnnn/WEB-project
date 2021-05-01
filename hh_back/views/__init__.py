52130870eda9a6dd64cce18b242ad04077b7de3a
Aruzhannnnn <64280949+Aruzhannnnn@users.noreply.github.com>
2021-05-01 15:11:15 +0600

    Delete hh_back/views directory

--- hh_back/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .views_fbv import tour_list, tour_detail
-# from .views_cbv import CategoryListAPIView, CategoryDetailAPIView
-# from .views_generic_v1 import CategoryListAPIView, CategoryDetailAPIView
-from .views_generic import TourListAPIView, TourDetailAPIView, \
-    AboutListAPIView, AboutDetailAPIView
